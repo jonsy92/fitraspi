1a9fcde282bc32ccb6cc0a39f5facf923f2d3976
Jonas <Jonas@DESKTOP-B4A4KFQ>
2016-09-07 18:59:02 +0200

    Neue Klasse CL_MySQL

---
+++ FitRaspi/FitRaspi/Cl_MySQL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace FitRaspi
+{
+    class Cl_MySQL
+    {
+        private static string connString;
+
+        public static string ConnString
+        {
+            get
+            {
+                return connString;
+            }
+
+            set
+            {
+                connString = value;
+            }
+        }
+
+        private static void OpenSQLConnection()
+        {
+         
+            using (SqlConnection connection = new SqlConnection(connString))
+            {
+                connection.Open();
+
+                Console.WriteLine("State: {0}", connection.State);
+                Console.WriteLine("ConnectionString: {0}",
+                    connection.ConnectionString);
+            }
+
+        }
+
+      
+    }
+}
