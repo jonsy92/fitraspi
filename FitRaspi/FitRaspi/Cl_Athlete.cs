72ba4ac71cf63d8d06de7e90cdefe621313ec319
Daniel <Daniel@DANIELS-PC>
2016-09-08 22:09:40 +0200

    die ersten Änderungen wie besprochen ;)

--- FitRaspi/FitRaspi/Cl_Athlete.cs
+++ FitRaspi/FitRaspi/Cl_Athlete.cs
@@ -23,34 +23,61 @@ namespace FitRaspi
         private double ffm;
         private double ffmi;
         private double kfa;
+        private double stomach;
+        private double neck;
+        private double waist;
+        private double butt;
 
-        public Cl_Athlete (string n, string f, string b, string e, string m, double w, double h, double ffm, double ffmi, double kfa)
+        public Cl_Athlete (string name, string firstname, string birthday, string email, string sex, double weight, double height, double ffm, double ffmi, double kfa, double stomach, double neck, double waist, double butt)
         {
-            this.name = n;
-            this.firstname = f;
-            this.birthday = b;
-            this.email = e;
-            this.sex = m;
-            this.weight = w;
-            this.height = h;
+            this.name = name;
+            this.firstname = firstname;
+            this.birthday = birthday;
+            this.email = email;
+            this.sex = sex;
+            this.weight = weight;
+            this.height = height;
             this.ffm = ffm;
             this.ffmi = ffmi;
             this.kfa = kfa;
+            this.stomach = stomach;
+            this.neck = neck;
+            this.waist = waist;
+            this.butt = butt;
+        }
+
+        public double calc_kfa(double stomach, double neck, double height)
+        {
+            double result;
+
+            result = 495 / (1.0324 - 0.19077 * Math.Log10(stomach - neck) + 0.15456 * Math.Log10(height)) - 450;
+          
+            return (result);
         }
 
         public double calc_kfa(double stomach, double neck, double height, double waist, double butt)
         {
             double result;
 
-            if (sex == "male")
-            {
-               result = 495 / (1.0324 - 0.19077 * Math.Log10(stomach - neck) + 0.15456 * Math.Log10(height)) - 450;
-            }
+            result = 495 / (1.29579 - 0.35004 * Math.Log10(stomach + waist - neck) + 0.22100 * Math.Log10(height)) - 450;
 
-            else
-            {
-                result = 495 / (1.29579 - 0.35004 * Math.Log10(stomach + waist - neck) + 0.22100 * Math.Log10(height)) - 450;
-            }
+            return (result);
+        }
+
+        public double calc_ffm(double weight, double kfa)
+        {
+            double result;
+
+            result = weight * (100 - kfa) / 100;
+
+            return (result);
+        }
+
+        public double calc_ffmi(double height, double ffm)
+        {
+            double result;
+
+            result = ffm / (height + height) + 6.3 * (1.8 - height);
 
             return (result);
         }
