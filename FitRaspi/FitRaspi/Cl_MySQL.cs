0b5878c7a78d91a7bf4f8e999e4d39e598adf95b
jonsy92 <jonas.niedrig@gmx.de>
2016-09-09 22:58:12 +0200

    Update Cl_MySQL.cs

--- FitRaspi/FitRaspi/Cl_MySQL.cs
+++ FitRaspi/FitRaspi/Cl_MySQL.cs
@@ -9,29 +9,23 @@ namespace FitRaspi
 {
     class Cl_MySQL
     {
-        
-        
-//        private static void OpenSQLConnection(string server,string userid, string password, string database )
-//        {
-         
-            MySqlConnectionStringBuilder conn_string = new MySqlConnectionStringBuilder();
-                conn_string.Server = server;
-                conn_string.UserID = userid;
-                conn_string.Password = password;
-                conn_string.Database = database;
-         
-         
-//            //using (SqlConnection connection = new SqlConnection(conn_string.ToString()))
-//            //{
-//            //    connection.Open();
+       public static void OpenSQLConnection(string server, string userid, string password, string database)
+        {
+            SqlConnectionStringBuilder conn_string = new SqlConnectionStringBuilder();
+            conn_string.DataSource = server;
+            conn_string.UserID = userid;
+            conn_string.Password = password;
+            conn_string.InitialCatalog = database;
 
-//            //    Console.WriteLine("State: {0}", connection.State);
-//            //    Console.WriteLine("ConnectionString: {0}",
-//            //        connection.ConnectionString);
-//            }
 
-//        }
+            using (SqlConnection connection = new SqlConnection(conn_string.ToString()))
+            {
+                connection.Open();
+                Console.WriteLine("State: {0}", connection.State);
+                Console.WriteLine("ConnectionString: {0}",
+                    connection.ConnectionString);
+            }
 
-      
-//    }
-//}
+    }
+    }      
+}
