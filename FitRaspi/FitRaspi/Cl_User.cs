5fbd006c4489d3ddfe97bc18a11bd75305bf8f1a
Jonas <Jonas@DESKTOP-B4A4KFQ>
2016-09-09 23:59:31 +0200

    Teilimplementierung der Klasse CL_User

---
+++ FitRaspi/FitRaspi/Cl_User.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitRaspi
+{
+    class Cl_User
+    {
+        private string userid;
+        private string password;
+        private string username;
+
+        public Cl_User(string username,string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public string get_uid(string username)
+        {
+            return ("Select UID...");
+        }
+
+
+        public string get_password(string userid)
+        {
+            return ("Select password...");
+        }
+
+        public bool check_password(string password)
+        {
+            //Check...
+            //if(password correct)
+            //{
+               return (true);
+            //}
+            //else
+            //{
+            //    return (false);
+            //}
+        }
+
+        public string Uid
+        {
+            get
+            {
+                return userid;
+            }
+
+            set
+            {
+                userid = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+
+            set
+            {
+                username = value;
+            }
+        }
+    }
+}
